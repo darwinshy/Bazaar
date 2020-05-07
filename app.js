2e41c811fb6f9f5362f3310102166c7c856f2019
Reverope <shatish123456789@gmail.com>
2020-05-07 11:44:49 +0530

    Runtime

--- app.js
+++ app.js
@@ -2,45 +2,42 @@ var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
-var methodOverride = require('method-override');
+var methodOverride = require("method-override");
 
-var User=require("./models/user");
-var Wishlist=require("./models/wishlist");
+var User = require("./models/user");
+// Uncomment when model has been created
+// var Wishlist = require("./models/wishlist");
 
+mongoose.connect(
+  "mongodb+srv://user:3wQN4sc3OGC3hlSc@cluster0-amz9t.gcp.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
 
-mongoose.connect("mongodb+srv://user:3wQN4sc3OGC3hlSc@cluster0-amz9t.gcp.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
-
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-
-
-app.get("/", function(req, res) {
-    res.redirect("/home")
+app.get("/", function (req, res) {
+  res.redirect("/home");
 });
 
-
-
-app.get("/home", function(req, res) {
-    res.render("homepage.ejs");
+app.get("/home", function (req, res) {
+  res.render("homepage.ejs");
 });
 
-
-app.get("/error", function(req, res) {
-    res.render("error.ejs");
+app.get("/error", function (req, res) {
+  res.render("error.ejs");
 });
 
 //Accounts Page
-app.get("/accounts", function(req, res) {
-    res.render("accounts.ejs");
+app.get("/accounts", function (req, res) {
+  res.render("accounts.ejs");
 });
 
-app.get("*", function(req, res) {
-    res.send("NO MATCH FOUND");
+app.get("*", function (req, res) {
+  res.send("NO MATCH FOUND");
 });
 
-app.listen(3000, function() {
-    console.log("SERVER STARTED");
-
+app.listen(3000, function () {
+  console.log("SERVER STARTED");
 });
